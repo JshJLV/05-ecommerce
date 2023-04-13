65339f7b2b2509f0774f382e9e790955d8fff393
Joseph Lopez <joseph.lpz1999@gmail.com>
2023-04-12 23:22:22 -0600

    boton de cantidad de producto agregado

--- src/pages/ProductView.js
+++ src/pages/ProductView.js
@@ -3,27 +3,40 @@ import TextDescription from "../components/ProductView/TextDescription";
 function ProductView() {
   return (
     <main>
-      <div className="h-72">
-        <img
-          className="max-h-full mx-auto"
-          src="https://cdn.gameplanet.com/wp-content/uploads/2022/08/20140206/portada-tears-of-the-kingdom-nsw-768x949.jpg"
-          alt=""
-        />
-      </div>
-      <div className="mt-4 p-4 gap-3">
-        <div>
-          <span className="text-lg">Titulo del juego</span>
+      <div className="xl:flex xl:h-96 py-2">
+        <div className="h-72 xl:h-96 xl:flex-1">
+          <img
+            className="max-h-full mx-auto"
+            src="https://cdn.gameplanet.com/wp-content/uploads/2022/08/20140206/portada-tears-of-the-kingdom-nsw-768x949.jpg"
+            alt=""
+          />
         </div>
-        <div className="text-lg mt-2">
-          <span>Precio del juego</span>
-        </div>
-        <div className="flex flex-col justify-around mt-2 gap-3">
-          <button className="bg-blue-500 text-white rounded-2xl p-2 hover:bg-blue-600">
-            Comprar
-          </button>
-          <button className="bg-blue-300 text-blue-500 rounded-2xl p-2 hover:bg-blue-400">
-            Añadir al carrito
-          </button>
+        <div className="mt-4 p-4 gap-3 xl:flex-1 xl:flex flex-col justify-around border-2">
+          <div className="text-lg mt-2 flex-col">
+            <span className="text-2xl block">Titulo del juego</span>
+            <span className="text-lg block font-bold">$Precio del juego</span>
+            <span className="text-sm block font-bold">
+              Envio gratis del producto de 3 a 5 dias
+            </span>
+          </div>
+          <div className="flex flex-col justify-around mt-2 gap-4 xl:w-80 content-end">
+            <label className="block">
+              Cantidad:
+              <select className="bg-gray-100 rounded-md p-2">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+            </label>
+            <button className="bg-blue-500 text-white rounded-2xl p-2 hover:bg-blue-600 hover:shadow-inner hover:shadow-blue-600">
+              Comprar
+            </button>
+            <button className="bg-blue-300 text-blue-500 rounded-2xl p-2 hover:bg-blue-400">
+              Añadir al carrito
+            </button>
+          </div>
         </div>
       </div>
       <TextDescription />
--- src/pages/User.js
+++ src/pages/User.js
@@ -1,5 +1,25 @@
 function User() {
-  return <div>User</div>;
+  return (
+    <main>
+      <div>Perfil de usuario:</div>
+      <form>
+        <label>Nombre: </label>
+        <input type="text" placeholder="" />
+        <label>Apellido: </label>
+        <input type="text" placeholder="" />
+        <label>codigo postal: </label>
+        <input type="text" placeholder="" />
+        <label>calle: </label>
+        <input type="text" placeholder="" />
+        <label>colonia: </label>
+        <input type="text" placeholder="" />
+        <label>estado: </label>
+        <input type="text" placeholder="" />
+        <label>email: </label>
+        <input type="email" placeholder="" />
+      </form>
+    </main>
+  );
 }
 
 export default User;
